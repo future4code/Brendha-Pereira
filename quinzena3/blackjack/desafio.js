12df3b53098d479f921648f957c980f97114f3cc
BrendhaLuizaB <barbosabrendhaluiza@gmail.com>
2021-04-10 16:08:33 -0300

    projeto-blackjack

---
+++ quinzena3/blackjack/desafio.js
@@ -0,0 +1,76 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+
+   //   let imported = document.createElement('script');
+   //   imported.src = 'nossoJogo.js';
+   //   document.head.appendChild(imported);  
+    
+      
+// let iniciarRodada
+//se for true -----> executa o programa abaixo
+// se for false executa o último else 
+// if (iniciarRodada) {
+
+// // //         let cartaUsuario = comprarCarta()
+// // //         let carta2Usuario = comprarCarta()
+// // //         let cartaComputador = comprarCarta()
+// // //         let carta2Computador = comprarCarta()
+        
+
+// // //         let pontosUsuario = cartaUsuario.valor + carta2Usuario.valor 
+// // //         let pontosComputador = cartaComputador.valor + carta2Computador.valor 
+
+// // //      confirm(
+// // //          `Suas cartas são ${cartaUsuario.texto} ${carta2Usuario.texto}. A carta revelada do computador é ${cartaComputador.texto}.` +
+// // //          "\n"+  // \n faz pular a linha
+// // //          "Deseja comprar mais uma carta?"
+// // //       )
+// // //       console.log(`Usuário - cartas: ${cartaUsuario.texto} ${carta2Usuario.texto} = ${pontosUsuario}`)
+// // //       console.log(`Computador - cartas: ${cartaComputador.texto} = ${pontosComputador}`)
+
+        
+      
+// // //          let cartaDeTeste = "A"
+// // //         if ((cartaUsuario ==="A" && carta2Usuario==="A") || (cartaComputador==="A" && carta2Computador=== "A")){
+// // //            confirm("Cartas iguais! Compre as cartas novamente")
+        
+
+// // //         let segundoSorteioUsuario = comprarCarta()
+// // //         let segundoSorteio2Usuario = comprarCarta()
+// // //         let segundoSorteioComputador = comprarCarta()
+// // //         let segundoSorteio2Computador = comprarCarta()
+
+// // //         let pontosSegundoSorteioUsuario = segundoSorteioUsuario.valor + segundoSorteio2Usuario.valor 
+// // //         let pontosSegundoSorteiocomputador = segundoSorteioComputador.valor + segundoSorteio2Computador.valor 
+        
+// // //         console.log(`Usuário - cartas: ${segundoSorteioUsuario.texto} ${segundoSorteio2Usuario.texto} = ${pontosSegundoSorteioUsuario}`)
+// // //         console.log(`Computador - cartas: ${segundoSorteioComputador.texto} ${segundoSorteio2Computador.texto} = ${pontosSegundoSorteiocomputador}`)
+// // //       }
+      
+
+
+// // //       //   if(pontosUsuario>pontosComputador){
+// // //       //      console.log("Usuário venceu!")
+// // //       //   }else if (pontosUsuario<pontosComputador){
+// // //       //      console.log("Computador venceu!")
+
+// // //       //   }else if (pontosComputador===pontosUsuario){
+// // //       //      console.log("Empate!")
+// // //       //   }
+
+       
+
+
+// // // // else {
+// // // //    console.log("Fim de jogo")
+// // // // }
